26033294a49467b2891fbee8b8e1e9473e042bbe
Scott <schneider_scott@live.com>
2018-08-01 15:08:08 -0600

    new keepr project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KeepsComponent } from './keeps/keeps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeepsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/keeps/keeps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keeps',
+  templateUrl: './keeps.component.html',
+  styleUrls: ['./keeps.component.css']
+})
+export class KeepsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
